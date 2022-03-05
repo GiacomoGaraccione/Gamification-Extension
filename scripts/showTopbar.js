63ce1958398b1e963a51d8086c76d3d51b5e0ccf
GiacomoGaraccione <homer4897@libero.it>
2022-03-05 17:17:42 +0100

    fix page coverage being sometimes computed incorrectly

--- scripts/showTopbar.js
+++ scripts/showTopbar.js
@@ -1,59 +1,35 @@
-chrome.storage.sync.get(["currentURL", "profileInfo"], function (result) {
+chrome.storage.sync.get(["currentURL", "profileInfo"], (result) => {
     let profileInfo = JSON.parse(result.profileInfo)
 
-    function filterLink(event) {
-        return event.objectType === "link"
+    function filterURL(event) {
+        return event.url === result.currentURL
     }
-    function filterInput(event) {
-        return event.objectType === "input"
-    }
-    function filterButton(event) {
-        return event.objectType === "button"
-    }
-    function filterSelect(event) {
-        return event.objectType === "select"
-    }
-
     chrome.runtime.sendMessage({
         mess: "fetch",
         method: "get",
-        body: "/pages/actions/" + profileInfo.username,
+        body: "/pages/records/" + profileInfo.username,
         content: { url: result.currentURL }
     }, (response) => {
-        let pageActions = response.data
+        let progress = response.data.filter(filterURL).length > 0 ? response.data.filter(filterURL)[0].coverage : 0
         chrome.runtime.sendMessage({
             mess: "fetch",
             method: "get",
             body: "/pages",
             content: { url: result.currentURL }
         }, (response2) => {
-            let pageInfo = response2.data[0]
-            let totalLinkObjects = pageInfo.totalLinkObjects;
-            let totalInputObjects = pageInfo.totalInputObjects;
-            let totalButtonObjects = pageInfo.totalButtonObjects;
-            let totalSelectObjects = pageInfo.totalSelectObjects
-            let denom = (totalLinkObjects + totalInputObjects + totalButtonObjects + totalSelectObjects) !== 0
-            let interactedLinks = pageActions.filter(filterLink).length;
-            let interactedInputs = pageActions.filter(filterInput).length;
-            let interactedButtons = pageActions.filter(filterButton).length;
-            let interactedSelects = pageActions.filter(filterSelect).length
-            let progress = denom ? ((interactedLinks + interactedInputs + interactedButtons + interactedSelects) * 100) / (totalLinkObjects + totalInputObjects + totalButtonObjects + totalSelectObjects) : -1;
-
+            let denom = (response2.data[0].totalLinkObjects + response2.data[0].totalInputObjects + response2.data[0].totalButtonObjects + response2.data[0].totalSelectObjects) !== 0
             let topnav = document.createElement("div");
             topnav.id = "gamificationExtensionTopnav";
             let found = document.getElementById("gamificationExtensionTopnav");
             if (found === null) {
                 document.body.appendChild(topnav);
-                topnav.style = "background-color: transparent;position: fixed;bottom: 0;width: 100%;";
+                topnav.style = "background-color: transparent;position: fixed;bottom: 10;width: 100%;";
                 let outerDiv = document.createElement("div");
                 outerDiv.id = "gamificationExtensionTopnavOuter";
                 outerDiv.style = "color:#000!important;background-color:#f1f1f1!important;border-radius:16px";
                 topnav.appendChild(outerDiv);
                 let innerDiv = document.createElement("div");
                 innerDiv.id = "gamificationExtensionTopnavInner";
-                if (progress === undefined) {
-                    progress = 0;
-                }
                 innerDiv.style = `border-radius:16px;margin-top:16px;margin-bottom:16px;color:#000!important;background-color:#2196F3!important; width:` + progress + `%; white-space:nowrap`;
                 innerDiv.textContent = denom ? "Progress: " + progress.toFixed(2) + "%" : "There are no widgets in this page";
                 outerDiv.appendChild(innerDiv);
